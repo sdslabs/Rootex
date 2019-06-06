e0954b65750264104127172aa63b5bd7a1887e06
Twarit <wtwarit@gmail.com>
2019-06-06 23:32:57 +0530

    Add Setup boilerplate

---
+++ Setup.bat
@@ -0,0 +1,10 @@
+git submodule init
+cd vcpkg
+if not exist vcpkg.exe ./bootstrap-vcpkg.bat
+./vcpkg.exe install glew:x64-windows
+./vcpkg.exe install glfw3:x64-windows
+cd ..
+mkdir Build
+cd Build
+cmake .. "-DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake"
+start Rootex.sln
