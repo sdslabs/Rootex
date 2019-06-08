880830de98af0b74d170a3985ab86395e917987a
Twarit <wtwarit@gmail.com>
2019-06-08 15:40:20 +0530

    Add engine as a library mode

---
+++ Game/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Main.h"
+#include "EventsModule/EventModule.h"
+
+int main()
+{
+	std::cin.get();
+	print();
+
+	return 0;
+}
--- Setup.bat
+++ Setup.bat
@@ -1,10 +1,11 @@
 git submodule init
 cd vcpkg
 if not exist vcpkg.exe ./bootstrap-vcpkg.bat
-./vcpkg.exe install glew:x64-windows
-./vcpkg.exe install glfw3:x64-windows
+vcpkg.exe install glew:x64-windows
+vcpkg.exe install glfw3:x64-windows
 cd ..
 mkdir Build
 cd Build
 cmake .. "-DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake"
 start Rootex.sln
+cd ..
