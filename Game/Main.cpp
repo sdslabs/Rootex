9fa1c649e4974d4dc74af7a3808870a5d220316f
IronicallySerious <wtwarit@gmail.com>
2019-06-13 01:13:44 +0530

    Add module distinction

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "Main.h"
-#include "EventsModule/EventModule.h"
+#include "Modules/EventsModule/EventModule.h"
 
 int main()
 {
---
+++ Source/ModuleManager/Module.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class ModuleManager;
+
+class Module
+{
+private:
+	Module();
+	~Module();
+
+	friend class ModuleManager;
+
+public:
+protected:
+};
