4e064049dbdf9868532f80a84aaa49d4d591ec11
Twarit <wtwarit@gmail.com>
2019-09-18 18:26:22 +0530

    Add better boilerplate

---
+++ Source/common.cpp
@@ -0,0 +1,29 @@
+#include "common.h"
+
+void print(const std::string& msg)
+{
+    std::cout << msg;
+}
+
+void printLine(const std::string& msg)
+{ 
+	std::cout << msg << std::endl;
+}
+
+void printWarning(const std::string& warning)
+{
+    std::cout << "\033[93m" << warning << "\033[0m" << std::endl;
+}
+
+void printError(const std::string& error)
+{
+    std::cout << "\033[91m" << error << "\033[0m" << std::endl;
+}
+
+void printIf(const bool& expr, const std::string& error)
+{
+    if (!expr) 
+	{
+        printError(error);
+    }
+}
---
+++ Source/common.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+void print(const std::string& msg);
+void printLine(const std::string& msg);
+
+void printWarning(const std::string& warning);
+void printError(const std::string& error);
+void printIf(const bool& expr, const std::string& error);
+
+#ifdef _DEBUG
+#define WARN(m_msg)          printWarning(std::string(__FILE__) + ":" + std::string(__FUNCSIG__) + ": " + m_msg);
+#define ERR(m_msg)           printError(std::string(__FILE__) + ":" + std::string(__FUNCSIG__) + ": " + m_msg);
+#define PANIC(m_expr, m_msg) printIf(m_expr, std::string(__FILE__) + ":" + std::string(__FUNCSIG__) + ": " + m_msg);
+
+#elif // TODO: Think about the behaviour in Release mode
+#define WARN(m_msg) printWarning(std::string(__FILE__) + ":" + std::string(__FUNCSIG__) + ": " + m_msg);
+#define ERR(m_msg) printError(std::string(__FILE__) + ":" + std::string(__FUNCSIG__) + ": " + m_msg);
+#define PANIC(m_expr, m_msg) printIf(m_expr, std::string(__FILE__) + ":" + std::string(__FUNCSIG__) + ": " + m_msg);
+#endif
