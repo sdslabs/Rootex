22087c0bafec9655a151fe6652bdc269d929781e
sin3point14 <manaschaudhary2000@gmail.com>
2019-11-17 06:20:11 +0530

    render white triangle

--- rootex/core/renderer/d3d11graphics.h
+++ rootex/core/renderer/d3d11graphics.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <d3d11.h>
 #include <wrl.h>
+#include <d3dcompiler.h>
+#include <array>
+#include <iostream>
 
 class RootexGraphics
 {
@@ -17,5 +20,6 @@ public:
 	~RootexGraphics() = default;
 
 	void ClearBuffer(float r, float g, float b);
+	void DrawTestTriangle();
 	void EndFrame();
 };
