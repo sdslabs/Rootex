072e57455f6eab3269b39445f47af94de17c0d3f
sin3point14 <manaschaudhary2000@gmail.com>
2019-11-17 07:05:03 +0530

    color interpolated triangle

--- rootex/core/renderer/shaders/VertexShader.hlsl
+++ rootex/core/renderer/shaders/VertexShader.hlsl
@@ -1,4 +1,13 @@
-float4 main( float2 pos : POSITION ) : SV_POSITION
+struct VSOut
 {
-	return float4(pos.x, pos.y, 0, 1.0);
+	float3 color : COLOR;
+	float4 pos : SV_POSITION;
+};
+
+VSOut main(float2 pos : POSITION, float3 color : COLOR)
+{
+	VSOut output;
+	output.pos = float4(pos.x, pos.y, 0, 1.0f);
+	output.color = color;
+	return output;
 }
