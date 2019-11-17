051789426e299836bbd8b7df14ec6bf651860f5e
sin3point14 <manaschaudhary2000@gmail.com>
2019-11-17 07:34:42 +0530

    add index buffer

--- rootex/core/renderer/rendering_device.cpp
+++ rootex/core/renderer/rendering_device.cpp
@@ -67,26 +67,52 @@ void RootexGraphics::DrawTestTriangle()
 		} color;
 	};
 	const Vertex vertices[] = {
-		{ 0.0f, 0.4f, 255, 0, 0, 0 },
-		{ 0.7f, 0.5f, 0, 255, 0, 0 },
-		{ -0.2f, -0.2f, 0, 0, 255, 0 }
+		{ 0.0f, 0.5f, 255, 0, 0, 0 },
+		{ 0.5f, -0.5f, 0, 255, 0, 0 },
+		{ -0.5f, -0.5f, 0, 0, 255, 0 },
+		{ -0.3f, 0.3f, 255, 0, 0, 0 },
+		{ 0.3f, 0.3f, 0, 255, 0, 0 },
+		{ 0.0f, -0.8f, 0, 0, 255, 0 }
 	};
+
 	Microsoft::WRL::ComPtr<ID3D11Buffer> pVertexBuffer;
-	D3D11_BUFFER_DESC desc = { 0 };
-	desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	desc.Usage = D3D11_USAGE_DEFAULT;
-	desc.CPUAccessFlags = 0u;
-	desc.MiscFlags = 0u;
-	desc.ByteWidth = sizeof(vertices);
-	desc.StructureByteStride = sizeof(Vertex);
-	D3D11_SUBRESOURCE_DATA sd = { 0 };
-	sd.pSysMem = vertices;
+	D3D11_BUFFER_DESC vbd = { 0 };
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.Usage = D3D11_USAGE_DEFAULT;
+	vbd.CPUAccessFlags = 0u;
+	vbd.MiscFlags = 0u;
+	vbd.ByteWidth = sizeof(vertices);
+	vbd.StructureByteStride = sizeof(Vertex);
+	D3D11_SUBRESOURCE_DATA vsd = { 0 };
+	vsd.pSysMem = vertices;
 
 	//create and bind vertex buffer
-	pDevice->CreateBuffer(&desc, &sd, &pVertexBuffer);
+
+	pDevice->CreateBuffer(&vbd, &vsd, &pVertexBuffer);
 	const UINT stride = sizeof(Vertex);
 	const UINT offset = 0u;
 	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+
+	//create and bind index buffer
+	const unsigned short indices[] = {
+		0,1,2,
+		0,2,3,
+		0,4,1,
+		2,1,5,
+	};
+	Microsoft::WRL::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = { 0 };
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(indices);
+	ibd.StructureByteStride = sizeof(unsigned short);
+	D3D11_SUBRESOURCE_DATA isd = { 0 };
+	isd.pSysMem = indices;
+
+	pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer);
+	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 	Microsoft::WRL::ComPtr<ID3DBlob> pBlob;
 
@@ -132,5 +158,5 @@ void RootexGraphics::DrawTestTriangle()
 	vp.TopLeftY = 0;
 	pContext->RSSetViewports(1u, &vp);
 
-	pContext->Draw((UINT)std::size(vertices), 0u);
+	pContext->DrawIndexed((UINT)std::size(indices), 0u, 0u);
 }
