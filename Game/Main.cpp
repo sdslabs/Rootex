79b88f2576af6460cf20108c658b7e8aedd164b5
murex971 <nupur202000@gmail.com>
2019-12-03 19:24:25 +0530

    Initial commit of task manager

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -2,23 +2,32 @@
 
 #include "core/audio/audio_manager.h"
 #include "core/renderer/window.h"
-#include "core/resource_loader.h"
 #include "core/resource_data_reserve.h"
+#include "core/resource_loader.h"
 #include "os/os.h"
+#include "os/thread.cpp"
+#include "os/thread.h"
 #include "script/interpreter.h"
 
 int main()
 {
 	OS::initialize();
 	OS::printLine("Rootex Engine is starting: Build(" + OS::getBuildDate() + "|" + OS::getBuildTime() + ")");
-	
+
 	// Engine starts from build/game/.
 	Ref<ResourceFile> r = ResourceLoader::createResourceFile(DirectoryShortcut::ENGINE, "Test File", "test/abc.txt", ResourceFile::Type::TXT); // So this loads build/game/abc.txt (However the binary exists in build/game/Debug/)
 	OS::printLine(r->getText());
-
 	Ref<ResourceFile> windowSettings = ResourceLoader::createResourceFile(DirectoryShortcut::GAME, "Window load script", "assets/config/window.lua", ResourceFile::Type::LUA);
 	OS::printLine(windowSettings->getText());
 
+	Vector<Ref<Task>> ju;
+	Ref<DebugTask> db(new DebugTask());
+	ju.push_back(db);
+	ThreadPool th;
+
+	th.threadPoolInitialise();
+	th.threadPoolSubmitTasks(ju);
+	
 	LuaInterpreter inter;
 	inter.loadExecuteScript(windowSettings);
 	LuaVariable window = inter.getGlobal("window");
