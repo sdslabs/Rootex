6bf3e907bfcbba3a15bb9770ffb9334ec0a22f09
Twarit <wtwarit@gmail.com>
2019-12-12 03:46:14 +0530

    Add VertexBuffer, IndexBuffer API

---
+++ rootex/core/renderer/opengl/opengl_vertex_buffer.cpp
@@ -0,0 +1,10 @@
+#include "opengl_vertex_buffer.h"
+
+OpenGLVertexBuffer::OpenGLVertexBuffer(Vector<char> buffer)
+    : VertexBuffer(buffer)
+{
+}
+
+OpenGLVertexBuffer::~OpenGLVertexBuffer()
+{
+}
