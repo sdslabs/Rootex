61fccdd6fedad82e6fcfa581430dddaab35887c6
Twarit <wtwarit@gmail.com>
2019-12-12 19:35:54 +0530

    Make renderer API non-singleton

--- rootex/core/renderer/renderer.cpp
+++ rootex/core/renderer/renderer.cpp
@@ -2,22 +2,3 @@
 
 #include "directx/directx_renderer.h"
 #include "opengl/opengl_renderer.h"
-
-Renderer::Renderer(RendererType type)
-{
-	switch (type)
-	{
-	case RendererType::DIRECTX:
-		//m_Instance = new DirectXRenderer();
-		break;
-	case RendererType::OPENGL:
-		//m_Instance = new OpenGLRenderer();
-		break;
-	default:
-		break;
-	}
-}
-
-Renderer::~Renderer()
-{
-}
