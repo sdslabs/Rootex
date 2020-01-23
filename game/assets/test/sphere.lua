32bb5dab9825a14c44f1789377a3f1967a6853e3
sin3point14 <manaschaudhary2000@gmail.com>
2020-01-23 21:20:12 +0530

    add point light entity, model

---
+++ game/assets/test/sphere.lua
@@ -0,0 +1,12 @@
+Entity = {
+    Components = {
+        TransformComponent = { 
+            m_Position = { x = 0, y = 0, z = 0 },
+            m_Rotation = { yaw = 0, pitch = 0, roll = 0 },
+            m_Scale = { x = 0.1, y = 0.1, z = 0.1 }
+        },
+        VisualComponent = {
+            resFile = "game/assets/sphere.obj",
+        }
+    }
+}
