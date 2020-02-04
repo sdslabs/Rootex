5c57fcaf66b39cfc86a48607a43a249863725a95
murex971 <nupur202000@gmail.com>
2020-02-04 20:53:04 +0530

    Basic properties and functions added to physics component

--- rootex/framework/components/physics/physics_component.cpp
+++ rootex/framework/components/physics/physics_component.cpp
@@ -1,12 +1,69 @@
 #include "physics_component.h"
+#include "core/resource_loader.h"
+#include "physics/physics.h"
 
 #include "entity.h"
 
-Component* PhysicsComponent::Create(const LuaVariable& componentData)
+PhysicsComponent::PhysicsComponent(btScalar mass, std::string& matName)
+    : mass(mass)
+    , m_MaterialName(matName)
+    , m_Material(lookUpMaterialData(m_MaterialName))
+    , m_MotionState(Matrix::Identity)
 {
-	PhysicsComponent* component = new PhysicsComponent();
+	m_TransformComponent = nullptr;
+	physicsMaterial = ResourceLoader::CreateLuaTextResourceFile("game/assets/config/physics.lua");
+	physicsLua.loadExecuteScript(physicsMaterial);
+	LuaVariable materialLua = physicsLua.getGlobal("m_MaterialName");
 
-	return component;
+	localInertia = btVector3(0.f, 0.f, 0.f);
+	if (mass > 0.f)
+	{
+		collisionShape->calculateLocalInertia(mass, localInertia);
+	}
+}
+
+bool PhysicsComponent::setup()
+{
+	bool status = true;
+	if (m_Owner)
+	{
+		m_TransformComponent = m_Owner->getComponent<TransformComponent>();
+		if (m_TransformComponent == nullptr)
+		{
+			status = false;
+		}
+		else
+		{
+			transform = m_TransformComponent->getTransform();
+
+			btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, &m_MotionState, collisionShape.get(), localInertia);
+
+			// set up the materal properties
+			rbInfo.m_restitution = m_Material.m_Restitution;
+			rbInfo.m_friction = m_Material.m_Friction;
+
+			body = new btRigidBody(rbInfo);
+
+			GamePhysics::GetSingleton()->addRigidBody(body);
+		}
+	}
+
+	return status;
+}
+
+PhysicsComponent::MotionState::MotionState(Matrix const& startingTransform)
+    : m_WorldToPositionTransform(startingTransform)
+{
+}
+
+void PhysicsComponent::MotionState::getWorldTransform(btTransform& worldTrans) const
+{
+	worldTrans = matTobtTransform(m_WorldToPositionTransform);
+}
+
+void PhysicsComponent::MotionState::setWorldTransform(const btTransform& worldTrans)
+{
+	m_WorldToPositionTransform = btTransformToMat(worldTrans);
 }
 
 PhysicsComponent::MaterialData::MaterialData(float restitution, float friction)
@@ -15,7 +72,50 @@ PhysicsComponent::MaterialData::MaterialData(float restitution, float friction)
 	m_Friction = friction;
 }
 
-btTransform PhysicsComponent::Mat4x4_to_btTransform(Matrix const& mat)
+float PhysicsComponent::lookUpSpecificGravity(const std::string& matName)
+{
+}
+
+PhysicsComponent::MaterialData PhysicsComponent::lookUpMaterialData(const std::string& materialName)
+{
+}
+
+void PhysicsComponent::applyForce(const Vector3 force)
+{
+	body->applyCentralImpulse(vecTobtVector3(force));
+}
+
+void PhysicsComponent::applyTorque(const Vector3 torque)
+{
+	body->applyTorqueImpulse(vecTobtVector3(torque));
+}
+
+void PhysicsComponent::setVelocity(const Vector3& velocity)
+{
+	body->setLinearVelocity(vecTobtVector3(velocity));
+}
+
+Vector3 PhysicsComponent::getVelocity()
+{
+	return btVector3ToVec(body->getLinearVelocity());
+}
+
+void PhysicsComponent::setAngularVelocity(const Vector3& angularVel)
+{
+	body->setAngularVelocity(vecTobtVector3(angularVel));
+}
+
+Vector3 PhysicsComponent::getAngularVelocity()
+{
+	return btVector3ToVec(body->getAngularVelocity());
+}
+
+void PhysicsComponent::translate(const Vector3& vec)
+{
+	body->translate(vecTobtVector3(vec));
+}
+
+btTransform PhysicsComponent::matTobtTransform(Matrix const& mat)
 {
 	// convert from Mat4x4 to btTransform
 	btMatrix3x3 bulletRotation;
@@ -42,4 +142,45 @@ btTransform PhysicsComponent::Mat4x4_to_btTransform(Matrix const& mat)
 	}
 
 	return btTransform(bulletRotation, bulletPosition);
+}
+
+Matrix PhysicsComponent::btTransformToMat(btTransform const& trans)
+{
+	Matrix returnValue = Matrix::Identity;
+
+	// convert from btTransform to Mat4x4
+	btMatrix3x3 const& bulletRotation = trans.getBasis();
+	btVector3 const& bulletPosition = trans.getOrigin();
+
+	// copy rotation matrix
+	for (int row = 0; row < 3; ++row)
+	{
+		for (int column = 0; column < 3; ++column)
+		{
+			returnValue.m[row][column] = bulletRotation[column][row];
+			// note the reversed indexing (row/column vs. column/row)
+			//  this is because Mat4x4s are row-major matrices and
+			//  btMatrix3x3 are column-major.  This reversed indexing
+			//  implicitly transposes (flips along the diagonal)
+			//  the matrix when it is copied.
+		}
+	}
+
+	// copy position
+	for (int column = 0; column < 3; ++column)
+	{
+		returnValue.m[3][column] = bulletPosition[column];
+	}
+
+	return returnValue;
+}
+
+btVector3 PhysicsComponent::vecTobtVector3(Vector3 const& vec3)
+{
+	return btVector3(vec3.x, vec3.y, vec3.z);
+}
+
+Vector3 PhysicsComponent::btVector3ToVec(btVector3 const& btvec)
+{
+	return Vector3(btvec.x(), btvec.y(), btvec.z());
 }
--- rootex/framework/components/physics/physics_component.h
+++ rootex/framework/components/physics/physics_component.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "component.h"
+#include"components/transform_component.h"
 
 #include"btBulletDynamicsCommon.h"
 
 class PhysicsComponent : public Component
 {
-	static Component* Create(const LuaVariable& componentData);
-
 	friend class EntityFactory;
 
 public:
@@ -15,16 +14,55 @@ public:
 
 	ComponentID getComponentID() const { return s_ID; }
 
+	PhysicsComponent(btScalar mass, std::string& matName);
+	~PhysicsComponent();
+
+	struct MotionState : public btMotionState
+	{
+		Matrix m_WorldToPositionTransform;
+
+		MotionState(Matrix const& startingTransform);
+
+		// btMotionState interface:  Bullet calls these
+		virtual void getWorldTransform(btTransform& worldTrans) const;
+
+		virtual void setWorldTransform(const btTransform& worldTrans);	
+	} m_MotionState;
+	
 	struct MaterialData
 	{
 		float m_Restitution;
 		float m_Friction;
 
 		MaterialData(float restitution, float friction);
-	};
+	} m_Material;
 
-	static btTransform Mat4x4_to_btTransform(Matrix const& mat);
+	Ptr<btCollisionShape> collisionShape;
+	btRigidBody* body;
 
-	static Matrix btTransform_to_Mat4x4(btTransform const& trans);
+	TransformComponent* m_TransformComponent;
+	bool setup() override;
+ 
+	Matrix transform;
+	btScalar mass;
+	btVector3 localInertia;	
+	float specificGravity;
+	float volume;
+	LuaInterpreter physicsLua;
+	LuaTextResourceFile* physicsMaterial;
+	std::string m_MaterialName;
 
+	static btTransform matTobtTransform(Matrix const& mat);
+	static Matrix btTransformToMat(btTransform const& trans);
+	static btVector3 vecTobtVector3(Vector3 const& vec3);
+	static Vector3 btVector3ToVec(btVector3 const& btvec);
+	float lookUpSpecificGravity(const std::string& matName);
+	MaterialData lookUpMaterialData(const std::string& materialName); 
+	void applyForce(const Vector3 force);
+	void applyTorque(const Vector3 torque);
+	void setVelocity(const Vector3& velocity);
+	Vector3 getVelocity();
+	void setAngularVelocity(const Vector3& angularVel);
+	Vector3 getAngularVelocity();
+	void translate(const Vector3& vec);
 };
