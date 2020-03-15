c28a08cf519dacfe748016f7bc991f4bade84019
Twarit <wtwarit@gmail.com>
2020-03-15 06:48:19 +0530

    Add play from editor feature

--- editor/gui/toolbar_dock.cpp
+++ editor/gui/toolbar_dock.cpp
@@ -2,6 +2,9 @@
 
 #include "editor/editor_application.h"
 
+#include "core/resource_loader.h"
+#include "core/renderer/texture.h"
+
 #include "vendor/ImGUI/imgui.h"
 #include "vendor/ImGUI/imgui_impl_dx11.h"
 #include "vendor/ImGUI/imgui_impl_win32.h"
@@ -12,6 +15,28 @@ void ToolbarDock::draw()
 	{
 		if (ImGui::Begin("Toolbar"))
 		{
+			ImGui::Columns(2);
+			
+			ImGui::Text("Play Level");
+			ImGui::SameLine();
+			if (ImGui::ArrowButton("Play Level", ImGuiDir_Right))
+			{
+			}
+
+			ImGui::NextColumn();
+		
+			ImGui::Text("Play Game");
+			ImGui::SameLine();
+			if (ImGui::ArrowButton("Play Game", ImGuiDir_Right))
+			{
+				EventManager::GetSingleton()->call("PreGameStartupSaveEvent", "EditorSaveAll", 0);
+				PRINT("Launched Game process");
+				OS::Execute("\"" + OS::GetGameExecutablePath() + "\"");
+				PRINT("Game process ended");
+			}
+
+			ImGui::Columns(1);
+
 			if (ImGui::TreeNodeEx("Editor", ImGuiTreeNodeFlags_CollapsingHeader))
 			{
 				float currentTime = EditorApplication::GetSingleton()->getAppFrameTimer().getTimeMs();
