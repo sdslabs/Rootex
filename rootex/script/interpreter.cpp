4d312a8747f7dfed7527deb3e062922179ead8de
Twarit <wtwarit@gmail.com>
2020-03-22 19:03:23 +0530

    Add visual/visual2d component bindings

--- rootex/script/interpreter.cpp
+++ rootex/script/interpreter.cpp
@@ -3,25 +3,75 @@
 
 #include "common/common.h"
 #include "components/transform_component.h"
+#include "components/hierarchy_component.h"
+#include "components/visual/visual_component.h"
+#include "components/visual/visual_2d_component.h"
+#include "components/visual/text_visual_2d_component.h"
 #include "entity_factory.h"
 #include "script/interpreter.h"
+
+void SolPanic(sol::optional<std::string> maybe_msg)
+{
+	std::cerr << "Lua is in a panic state and will now abort() the application" << std::endl;
+	if (maybe_msg)
+	{
+		const std::string& msg = maybe_msg.value();
+		ERR("\tError message: " + msg);
+	}
+}
+
+LuaInterpreter::LuaInterpreter()
+    : m_Lua(sol::c_call<decltype(&SolPanic), &SolPanic>)
+{
+	m_Lua.open_libraries(sol::lib::base);
+	m_Lua.open_libraries(sol::lib::io);
+	m_Lua.open_libraries(sol::lib::math);
+	m_Lua.open_libraries(sol::lib::os);
+	m_Lua.open_libraries(sol::lib::string);
+	m_Lua.open_libraries(sol::lib::table);
+
+	registerTypes();
+}
+
+LuaInterpreter* LuaInterpreter::GetSingleton()
+{
+	static LuaInterpreter singleton;
+	return &singleton;
+}
 
 void LuaInterpreter::registerTypes()
 {
 	sol::table rootex = m_Lua.create_named_table("Rootex");
 	{
-		sol::usertype<Vector2> vector2 = rootex.new_usertype<Vector2>("Vector2", sol::constructors<Vector2(), Vector2(float, float)>());
+		sol::usertype<Vector2> vector2 = rootex.new_usertype<Vector2>(
+			"Vector2", 
+			sol::constructors<Vector2(), Vector2(float, float)>(),
+		    sol::meta_function::addition, [](Vector2& l, Vector2& r) { return l + r; },
+		    sol::meta_function::subtraction, [](Vector2& l, Vector2& r) { return l - r; });
+		vector2["dot"] = &Vector2::Dot;
+		vector2["cross"] = [](const Vector2& l, const Vector2& r) { return l.Cross(r); };
 		vector2["x"] = &Vector2::x;
 		vector2["y"] = &Vector2::y;
 	}
 	{
-		sol::usertype<Vector3> vector3 = rootex.new_usertype<Vector3>("Vector3", sol::constructors<Vector3(), Vector3(float, float, float)>());
+		sol::usertype<Vector3> vector3 = rootex.new_usertype<Vector3>(
+			"Vector3", 
+			sol::constructors<Vector3(), Vector3(float, float, float)>(),
+		    sol::meta_function::addition, [](Vector3& l, Vector3& r) { return l + r; },
+		    sol::meta_function::subtraction, [](Vector3& l, Vector3& r) { return l - r; });
+		vector3["dot"] = &Vector3::Dot;
+		vector3["cross"] = [](const Vector3& l, const Vector3& r) { return l.Cross(r); };
 		vector3["x"] = &Vector3::x;
 		vector3["y"] = &Vector3::y;
 		vector3["z"] = &Vector3::z;
 	}
 	{
-		sol::usertype<Vector4> vector4 = rootex.new_usertype<Vector4>("Vector4", sol::constructors<Vector4(), Vector4(float, float, float, float)>());
+		sol::usertype<Vector4> vector4 = rootex.new_usertype<Vector4>(
+		    "Vector4", sol::constructors<Vector4(), Vector4(float, float, float, float)>(),
+		    sol::meta_function::addition, [](Vector4& l, Vector4& r) { return l + r; },
+		    sol::meta_function::subtraction, [](Vector4& l, Vector4& r) { return l - r; });
+		vector4["dot"] = &Vector4::Dot;
+		vector4["cross"] = [](const Vector4& l, const Vector4& r) { return l.Cross(l, r); };
 		vector4["x"] = &Vector4::x;
 		vector4["y"] = &Vector4::y;
 		vector4["z"] = &Vector4::z;
@@ -42,7 +92,12 @@ void LuaInterpreter::registerTypes()
 		quaternion["w"] = &Quaternion::w;
 	}
 	{
-		sol::usertype<Matrix> matrix = rootex.new_usertype<Matrix>("Matrix", sol::constructors<Matrix()>());
+		sol::usertype<Matrix> matrix = rootex.new_usertype<Matrix>(
+			"Matrix", 
+			sol::constructors<Matrix()>(),
+		    sol::meta_function::addition, [](Matrix& l, Matrix& r) { return l + r; },
+		    sol::meta_function::subtraction, [](Matrix& l, Matrix& r) { return l - r; },
+		    sol::meta_function::multiplication, [](Matrix& l, Matrix& r) { return l * r; });
 		matrix["Identity"] = sol::var(Matrix::Identity);
 	}
 	{
@@ -104,9 +159,8 @@ void LuaInterpreter::registerTypes()
 	}
 	{
 		sol::usertype<EntityFactory> entityFactory = rootex.new_usertype<EntityFactory>("EntityFactory");
-		entityFactory["Get"] = &EntityFactory::GetSingleton;
-		entityFactory["create"] = &EntityFactory::createEntity;
-		entityFactory["find"] = &EntityFactory::findEntity;
+		entityFactory["Create"] = [](TextResourceFile* t) { return EntityFactory::GetSingleton()->createEntity(t); };
+		entityFactory["Find"] = [](EntityID e) { return EntityFactory::GetSingleton()->findEntity(e); };
 	}
 	{
 		sol::usertype<Entity> entity = rootex.new_usertype<Entity>("Entity");
@@ -123,39 +177,62 @@ void LuaInterpreter::registerTypes()
 		component["getComponentID"] = &Component::getComponentID;
 		component["getName"] = &Component::getName;
 
-		sol::usertype<TransformComponent> transformComponent = rootex.new_usertype<TransformComponent>(
-		    "TransformComponent",
-		    sol::base_classes, sol::bases<Component>());
-		entity["getTransform"] = [](Entity* e) { return e->getComponent<TransformComponent>(); };
-		transformComponent["setPosition"] = &TransformComponent::setPosition;
-		transformComponent["setRotation"] = &TransformComponent::setRotation;
-		transformComponent["setScale"] = &TransformComponent::setScale;
-		transformComponent["setTransform"] = &TransformComponent::setTransform;
-		transformComponent["addTransform"] = &TransformComponent::addTransform;
-		transformComponent["getPosition"] = &TransformComponent::getPosition;
-		transformComponent["getRotation"] = &TransformComponent::getRotation;
-		transformComponent["getScale"] = &TransformComponent::getScale;
-		transformComponent["getLocalTransform"] = &TransformComponent::getLocalTransform;
-		transformComponent["getAbsoluteTransform"] = &TransformComponent::getAbsoluteTransform;
-		transformComponent["getComponentID"] = &TransformComponent::getComponentID;
-		transformComponent["getName"] = &TransformComponent::getName;
+		{
+			sol::usertype<TransformComponent> transformComponent = rootex.new_usertype<TransformComponent>(
+			    "TransformComponent",
+			    sol::base_classes, sol::bases<Component>());
+			entity["getTransform"] = [](Entity* e) { return e->getComponent<TransformComponent>(); };
+			transformComponent["setPosition"] = &TransformComponent::setPosition;
+			transformComponent["setRotation"] = &TransformComponent::setRotation;
+			transformComponent["setScale"] = &TransformComponent::setScale;
+			transformComponent["setTransform"] = &TransformComponent::setTransform;
+			transformComponent["addTransform"] = &TransformComponent::addTransform;
+			transformComponent["getPosition"] = &TransformComponent::getPosition;
+			transformComponent["getRotation"] = &TransformComponent::getRotation;
+			transformComponent["getScale"] = &TransformComponent::getScale;
+			transformComponent["getLocalTransform"] = &TransformComponent::getLocalTransform;
+			transformComponent["getAbsoluteTransform"] = &TransformComponent::getAbsoluteTransform;
+			transformComponent["getComponentID"] = &TransformComponent::getComponentID;
+			transformComponent["getName"] = &TransformComponent::getName;
+		}
+		{
+			sol::usertype<HierarchyComponent> hierarchyComponent = rootex.new_usertype<HierarchyComponent>(
+			    "HierarchyComponent",
+			    sol::base_classes, sol::bases<Component>());
+			entity["getHierarchy"] = [](Entity* e) { return e->getComponent<HierarchyComponent>(); };
+			hierarchyComponent["addChild"] = &HierarchyComponent::addChild;
+			hierarchyComponent["removeChild"] = &HierarchyComponent::removeChild;
+			hierarchyComponent["clear"] = &HierarchyComponent::clear;
+			hierarchyComponent["getParent"] = &HierarchyComponent::getParent;
+			hierarchyComponent["getChildren"] = &HierarchyComponent::getChildren;
+		}
+		{
+			sol::usertype<VisualComponent> visualComponent = rootex.new_usertype<VisualComponent>(
+			    "VisualComponent",
+			    sol::base_classes, sol::bases<Component>());
+			entity["getVisual"] = [](Entity* e) { return e->getComponent<VisualComponent>(); };
+			visualComponent["addTransform"] = &VisualComponent::addTransform;
+			visualComponent["getAttributes"] = &VisualComponent::getAttributes;
+			visualComponent["isVisible"] = &VisualComponent::isVisible;
+			visualComponent["getPosition"] = &VisualComponent::getPosition;
+			visualComponent["setTransform"] = &VisualComponent::setTransform;
+			visualComponent["setMaterial"] = &VisualComponent::setMaterial;
+			visualComponent["setPosition"] = &VisualComponent::setPosition;
+			visualComponent["setVisibility"] = &VisualComponent::setVisibility;
+		}
+		{
+			sol::usertype<Visual2DComponent> visual2DComponent = rootex.new_usertype<Visual2DComponent>(
+			    "Visual2DComponent",
+			    sol::base_classes, sol::bases<Component, VisualComponent>());
+			entity["getVisual2D"] = [](Entity* e) { return e->getComponent<Visual2DComponent>(); };
+		}
+		{
+			sol::usertype<TextVisual2DComponent> textVisual2DComponent = rootex.new_usertype<TextVisual2DComponent>(
+			    "TextVisual2DComponent",
+			    sol::base_classes, sol::bases<Component, VisualComponent, Visual2DComponent>());
+			entity["getTextVisual2D"] = [](Entity* e) { return e->getComponent<TextVisual2DComponent>(); };
+			textVisual2DComponent["setFont"] = &TextVisual2DComponent::setFont;
+			textVisual2DComponent["setText"] = &TextVisual2DComponent::setText;
+		}
 	}
-}
-
-LuaInterpreter::LuaInterpreter()
-{
-	m_Lua.open_libraries(sol::lib::base);
-	m_Lua.open_libraries(sol::lib::io);
-	m_Lua.open_libraries(sol::lib::math);
-	m_Lua.open_libraries(sol::lib::os);
-	m_Lua.open_libraries(sol::lib::string);
-	m_Lua.open_libraries(sol::lib::table);
-
-	registerTypes();
-}
-
-LuaInterpreter* LuaInterpreter::GetSingleton()
-{
-	static LuaInterpreter singleton;
-	return &singleton;
 }
