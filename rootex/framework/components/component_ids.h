fabedc401998c7fa21b8ad5415e0c42f4bf7e230
Twarit <wtwarit@gmail.com>
2020-04-02 01:21:41 +0530

    Separate out ModelVisualComponent

--- rootex/framework/components/component_ids.h
+++ rootex/framework/components/component_ids.h
@@ -7,6 +7,7 @@ enum class ComponentIDs : unsigned int
 	DebugComponent,
 	VisualComponent,
 	CPUParticlesVisualComponent = VisualComponent,
+	ModelVisualComponent = VisualComponent,
 	DiffuseVisualComponent = VisualComponent,
 	Visual2DComponent = VisualComponent,
 	TextVisual2DComponent = Visual2DComponent,
