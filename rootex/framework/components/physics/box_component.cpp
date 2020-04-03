234d8780cadd58b220c731d6bb659b540344d112
murex971 <nupur202000@gmail.com>
2020-04-03 09:37:14 +0530

    Adds box-component

---
+++ rootex/framework/components/physics/box_component.cpp
@@ -0,0 +1,40 @@
+#include "box_component.h"
+
+BoxComponent::BoxComponent(Vector3 dimensions, const String& matName)
+    : PhysicsComponent(matName, dimensions.x * dimensions.y * dimensions.z, Ref<btBoxShape>(new btBoxShape(vecTobtVector3(dimensions))))
+    , m_Dimensions(dimensions)
+{
+	if (m_Mass > 0.f)
+	{
+		m_CollisionShape->calculateLocalInertia(m_Mass, m_LocalInertia);
+	}
+}
+
+JSON::json BoxComponent::getJSON() const
+{
+	JSON::json j;
+
+	j["dimensions"]["x"] = m_Dimensions.x;
+	j["dimensions"]["y"] = m_Dimensions.y;
+	j["dimension.z"]["z"] = m_Dimensions.z;
+
+	j["matName"] = m_MaterialName;
+
+	return j;
+}
+
+Component* BoxComponent::Create(const JSON::json& boxComponentData)
+{
+	BoxComponent* component = new BoxComponent(
+		{ boxComponentData["dimensions"]["x"], boxComponentData["dimensions"]["y"], boxComponentData["dimensions"]["z"] },
+		boxComponentData["matName"]);
+	return component;
+}
+
+Component* BoxComponent::CreateDefault()
+{
+	BoxComponent* component = new BoxComponent(
+		{ 1.0f, 1.0f, 1.0f },
+		"Air");
+	return component;
+}
