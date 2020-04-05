160d33141569725b9a71d11aee4d4eb0102ee895
Angad Kambli <angad-k@users.noreply.github.com>
2020-04-06 02:06:38 +0530

    Initial commit

--- rootex/framework/components/visual/camera_visual_component.cpp
+++ rootex/framework/components/visual/camera_visual_component.cpp
@@ -1,18 +1,56 @@
 #include "camera_visual_component.h"
+#include "systems/render_system.h"
+Component* CameraVisualComponent::Create(const JSON::json& componentData)
+{
+	CameraVisualComponent* cameraVisualComponent = new CameraVisualComponent(
+	    { componentData["position"]["x"], componentData["position"]["y"], componentData["position"]["z"] },
+	    { componentData["direction"]["x"], componentData["direction"]["y"], componentData["direction"]["z"] });
 
-#include "entity_factory.h"
+	Ref<CameraVisualComponent> cameraPointer(cameraVisualComponent);
+
+	RenderSystem::GetSingleton()->setCamera(cameraPointer);
+
+	return cameraVisualComponent;
+}
+
+Component* CameraVisualComponent::CreateDefault()
+{
+	CameraVisualComponent* cameraVisualComponent = new CameraVisualComponent(
+	    { 0.0f, 0.0f, 4.0f },
+	    { 0.0f, 0.0f, -1.0f });
+	return cameraVisualComponent;
+}
+
+CameraVisualComponent::CameraVisualComponent(const Vector3& position,const Vector3& direction)
+    : VisualComponent(RenderPassMain, nullptr, nullptr, false)
+    , m_DebugCamera(false)
+    , m_ViewMatrix(Matrix::CreateLookAt(position, position + direction, { 0.0f, 1.0f, 0.0f }))
+    , m_ProjectionMatrix(Matrix::CreatePerspective(1.0f, 1.0f * 480.0f / 640.0f, 0.5f, 100.0f))
+    , m_Active(true)
+    , m_CameraOffset(0.0f, 1.0f, -10.0f, 0.0f)
+    , m_Position(position)
+    , m_Direction(direction)
+{
+	if (m_Owner)
+	{
+		//m_TransformComponent = m_Owner->getComponent<TransformComponent>();
+	}
+}
 
 CameraVisualComponent::CameraVisualComponent()
     : VisualComponent(RenderPassMain, nullptr, nullptr, false)
     , m_DebugCamera(false)
-    , m_ViewMatrix(Matrix::CreateLookAt({ 0.0f, 0.0f, 4.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }))
+    , m_ViewMatrix(Matrix::CreateLookAt({ 0.0f, 0.0f, 0.4f }, {0.0f, 0.0f, 0.3f}, { 0.0f, 1.0f, 0.0f }))
     , m_ProjectionMatrix(Matrix::CreatePerspective(1.0f, 1.0f * 480.0f / 640.0f, 0.5f, 100.0f))
     , m_Active(true)
-    , m_Target(nullptr)
     , m_CameraOffset(0.0f, 1.0f, -10.0f, 0.0f)
     , m_Position(0.0f, 0.0f, 4.0f)
     , m_Direction(0.0f, 0.0f, -1.0f)
 {
+	if (m_Owner)
+	{
+		//m_TransformComponent = m_Owner->getComponent<TransformComponent>();
+	}
 }
 
 CameraVisualComponent::~CameraVisualComponent()
@@ -21,6 +59,11 @@ CameraVisualComponent::~CameraVisualComponent()
 
 bool CameraVisualComponent::preRender()
 {
+	if (m_TransformComponent)
+	{
+		m_Position = m_TransformComponent->getPosition();
+		m_ViewMatrix = Matrix::CreateLookAt(m_Position, m_Position + m_Direction, { 0.0f, 1.0f, 0.0f });
+	}
 	return true;
 }
 
@@ -52,6 +95,12 @@ void CameraVisualComponent::setPosition(Vector3 position)
 {
 	m_Position = position;
 	m_ViewMatrix = Matrix::CreateLookAt(m_Position, m_Position + m_Direction, { 0.0f, 1.0f, 0.0f });
+}
+
+void CameraVisualComponent::updatePosition()
+{
+	m_TransformComponent = m_Owner->getComponent<TransformComponent>();
+	m_Position = m_TransformComponent->getPosition();
 }
 
 void CameraVisualComponent::setViewTransform(const Matrix& view)
--- rootex/framework/components/visual/camera_visual_component.h
+++ rootex/framework/components/visual/camera_visual_component.h
@@ -10,11 +10,15 @@ protected:
 	bool m_Active;
 	bool m_DebugCamera;
 	Ref<VisualComponent> m_Target;
+	Ref<TransformComponent> m_TransformComponent;
 	Vector4 m_CameraOffset;
 	Vector3 m_Position;
 	Vector3 m_Direction;
 
 public:
+	static Component* Create(const JSON::json& componentData);
+	static Component* CreateDefault();
+	CameraVisualComponent(const Vector3& position, const Vector3& direction);
 	CameraVisualComponent();
 	CameraVisualComponent(CameraVisualComponent&) = delete;
 	~CameraVisualComponent();
@@ -27,6 +31,7 @@ public:
 
 	virtual void setViewTransform(const Matrix& view);
 	void setPosition(Vector3 position);
+	void updatePosition();
 	virtual void setTarget(Ref<VisualComponent> target) { m_Target = target; }
 
 	virtual VisualComponent* getTarget() const { return m_Target.get(); }
@@ -34,4 +39,7 @@ public:
 	virtual const Matrix& getProjection() const { return m_ProjectionMatrix; }
 	void setOffset(const Vector4& offset) { m_CameraOffset = offset; }
 	virtual String getName() const override { return "CameraVisualComponent"; }
+
+	static const ComponentID s_ID = (ComponentID)ComponentIDs::CameraVisualComponent;
+	ComponentID getComponentID() const { return s_ID; }
 };
