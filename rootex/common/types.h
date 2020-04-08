e67dd6608f8e770a9a9e2281618fc9afefe94f26
sin3point14 <manaschaudhary2000@gmail.com>
2020-04-08 23:16:40 +0530

    add docs for common

--- rootex/common/types.h
+++ rootex/common/types.h
@@ -2,71 +2,97 @@
 
 // Smart pointers
 #include <memory>
+/// std::unique_ptr
 template <class T>
 using Ptr = std::unique_ptr<T>;
+/// std::shared_ptr
 template <class T>
 using Ref = std::shared_ptr<T>;
+/// std::weak_ptr
 template <class T>
 using Weak = std::weak_ptr<T>;
 #include <wrl.h> // For using Microsoft::WRL::ComPtr<T>
 
 // Serialization streams
 #include <fstream>
+/// std::fstream
 typedef std::fstream InputOutputFileStream;
+/// std::ofstream
 typedef std::ofstream OutputFileStream;
+/// std::ifstream
 typedef std::ifstream InputFileStream;
 #include <sstream>
+/// std::stringstream
 typedef std::stringstream StringStream;
 
 // Containers
 #include <string>
+/// std::string
 typedef std::string String;
 
 #include <map>
+/// std::map
 template <class P, class Q>
 using Map = std::map<P, Q>;
 
 #include <unordered_map>
+///std::unordered_map
 template <class P, class Q>
 using HashMap = std::unordered_map<P, Q>;
 
 #include <utility>
+/// std::tuple
 template <typename...P>
 using Tuple = std::tuple<P...>;
+/// std::pair
 template <class P, class Q>
 using Pair = std::pair<P, Q>;
 
 #include <vector>
+/// std::vector
 template <class T>
 using Vector = std::vector<T>;
 
 #include <filesystem>
+/// std::filesystem::path
 using FilePath = std::filesystem::path;
 
 // Math Containers
 #include <d3d11.h>
 #include "vendor/DirectXTK/Inc/SimpleMath.h"
+/// DirectX::SimpleMath::Matrix
 typedef DirectX::SimpleMath::Matrix Matrix;
+/// DirectX::SimpleMath::Vector2
 typedef DirectX::SimpleMath::Vector2 Vector2;
+/// DirectX::SimpleMath::Vector3
 typedef DirectX::SimpleMath::Vector3 Vector3;
+/// DirectX::SimpleMath::Vector4
 typedef DirectX::SimpleMath::Vector4 Vector4;
+/// DirectX::SimpleMath::Quaternion
 typedef DirectX::SimpleMath::Quaternion Quaternion;
+/// DirectX::SimpleMath::Color
 typedef DirectX::SimpleMath::Color Color;
 
 #include <DirectXColors.h>
+/// DirectX::Colors
 namespace ColorPresets = DirectX::Colors;
 
 #include <variant>
+/// Vector of std::variant of bool, int, char, float, String, Vector2, Vector3, Vector4, Matrix
 typedef Vector<std::variant<bool, int, char, float, String, Vector2, Vector3, Vector4, Matrix>> VariantVector;
 class Entity;
+/// std::variant of bool, int, char, float, String, Vector2, Vector3, Vector4, Matrix VariantVector, Ref<Entity>, Vector<String>
 using Variant = std::variant<bool, int, char, float, String, Vector2, Vector3, Vector4, Matrix, VariantVector, Ref<Entity>, Vector<String>>;
+/// extract the value of type TypeName from a Variant
 #define Extract(TypeName, variant) std::get<TypeName>((variant))
 
 #include <functional>
+/// std::function
 template <class T>
 using Function = std::function<T>;
 
 #include "JSON/json.hpp"
+/// namespace for the JSON library
 namespace JSON = nlohmann;
 
 #include "imgui.h"
