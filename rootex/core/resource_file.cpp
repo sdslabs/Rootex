0af438260de9d2aefd367d8893a6f49a0d96c167
allrounder27 <gautambafna2708@gmail.com>
2020-04-13 15:11:12 +0530

    Compare ptr to nullptr instead of false

--- rootex/core/resource_file.cpp
+++ rootex/core/resource_file.cpp
@@ -203,6 +203,7 @@ void VisualModelResourceFile::reload()
 		  aiProcess_Triangulate           |   
 		  aiProcess_JoinIdenticalVertices | 
 		  aiProcess_SortByPType);
+
 	PANIC(scene == nullptr, "Model could not be loaded: " + OS::GetAbsolutePath(m_ResourceData->getPath().string()).generic_string());
 
     const aiMesh* mesh = scene->mMeshes[0];
