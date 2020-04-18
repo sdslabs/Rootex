733354a6709ae65402a31098e149eb14ff01de62
Twarit <wtwarit@gmail.com>
2020-04-19 01:28:31 +0530

    Add translation gizmo to editor viewport using ImGuizmo

--- editor/gui/viewport_dock.cpp
+++ editor/gui/viewport_dock.cpp
@@ -1,7 +1,12 @@
 #include "viewport_dock.h"
 
 #include "renderer/rendering_device.h"
+#include "framework/systems/render_system.h"
+
 #include "editor/editor.h"
+#include "editor/gui/inspector_dock.h"
+
+#include "ImGuizmo.h"
 
 ViewportDock::ViewportDock(const JSON::json& viewportJSON)
 {
@@ -32,6 +37,31 @@ void ViewportDock::draw()
 			    { 1, 1 },
 			    m_ViewportDockSettings.m_ImageTint,
 			    m_ViewportDockSettings.m_ImageBorderColor);
+
+			static ImVec2 rect;
+			rect = ImGui::GetWindowSize();
+			static ImVec2 pos;
+			pos = ImGui::GetWindowPos();
+			ImGuizmo::SetRect(pos.x, pos.y, rect.x, rect.y);
+
+			Ref<Entity> openedEntity = InspectorDock::GetSingleton()->getOpenedEntity();
+			if (openedEntity && openedEntity->getComponent<TransformComponent>())
+			{
+				Matrix view = RenderSystem::GetSingleton()->getCamera()->getViewMatrix();
+				Matrix proj = RenderSystem::GetSingleton()->getCamera()->getProjectionMatrix();
+				Matrix matrix = openedEntity->getComponent<TransformComponent>()->getAbsoluteTransform();
+				float snap[3] = { 1.0f, 1.0f, 1.0f };
+
+				ImGuizmo::Manipulate(
+				    &view.m[0][0],
+				    &proj.m[0][0],
+				    ImGuizmo::OPERATION::TRANSLATE,
+				    ImGuizmo::MODE::LOCAL,
+				    &matrix.m[0][0],
+				    0,
+				    snap);
+				openedEntity->getComponent<TransformComponent>()->setTransform(matrix);
+			}
 		}
 		ImGui::End();
 	}
