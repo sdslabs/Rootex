8958fc53449240c406a87fc630e1d318cc593eb2
Meet <Meet@DESKTOP-8LKEJ4A>
2020-05-24 04:54:37 +0530

    add localization

---
+++ rootex/core/locale.cpp
@@ -0,0 +1,32 @@
+#include "locale.h"
+#include "common.h"
+
+Locale* Locale::GetSingleton()
+{
+	static Locale singleton;
+	return &singleton;
+}
+
+void Locale::LoadLanguage(String location)
+{
+	if (OS::IsFile(location))
+	{
+		JSON::json file;
+		std::ifstream i(location);
+		i >> file;
+		m_GameStrings = file.get<std::unordered_map<String, String>>();
+	}
+}
+
+String Locale::GetString(String key)
+{
+	return m_GameStrings.at(key);
+}
+
+Locale::Locale()
+{
+}
+
+Locale ::~Locale()
+{
+}
