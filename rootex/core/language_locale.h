be5d988553beb7ac0e4a9d32addbb4484447a4e2
Meet Shah <meetcshah19@gmail.com>
2020-05-25 02:54:29 +0530

    add documentation

---
+++ rootex/core/language_locale.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "common/common.h"
+// Loads game strings from a .json file.\n 
+// The expected format for the .json file consisting of game strings is {"key1":"game string 1" , "key2":"game string 2"}
+class Locale
+{
+	HashMap<String, String> m_GameStrings;
+	Locale() = default;
+
+public:
+	static Locale* GetSingleton();
+
+	// Used to get game string of the current language by passing the key defined for the string in the .json file
+	String GetString(const String key);
+	// Used to load game strings from the .json file present in the location passed 
+	void LoadLanguage(const String location);
+};
