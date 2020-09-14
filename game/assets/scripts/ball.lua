ab9cc7a7ff5cd62fedb6448208f4e8a356b4361c
Twarit Waikar <wtwarit@gmail.com>
2020-09-14 06:30:31 +0530

    Fix overhead level to use the new RTX namespace

--- game/assets/scripts/ball.lua
+++ game/assets/scripts/ball.lua
@@ -3,20 +3,20 @@ function onBegin(entity)
 end
 
 function onUpdate(entity, delta)
-    if InputManager.Get():isPressed("Forward") then
-       ball:applyForce(Vector3.new(0, 0, 0.002))
+    if RTX.InputManager.Get():isPressed("Forward") then
+       ball:applyForce(RTX.Vector3.new(0, 0, 0.002))
     end
-    if InputManager.Get():isPressed("Backward") then
-        ball:applyForce(Vector3.new(0, 0, -0.002)) 
+    if RTX.InputManager.Get():isPressed("Backward") then
+        ball:applyForce(RTX.Vector3.new(0, 0, -0.002)) 
     end
-    if InputManager.Get():isPressed("Left") then
-        ball:applyForce(Vector3.new(-0.002, 0.0, 0.0)) 
+    if RTX.InputManager.Get():isPressed("Left") then
+        ball:applyForce(RTX.Vector3.new(-0.002, 0.0, 0.0)) 
     end
-    if InputManager.Get():isPressed("Right") then
-        ball:applyForce(Vector3.new(0.002, 0, 0)) 
+    if RTX.InputManager.Get():isPressed("Right") then
+        ball:applyForce(RTX.Vector3.new(0.002, 0, 0)) 
     end
-    if InputManager.Get():isPressed("Jump") then
-        ball:setVelocity(ball:getVelocity() + Vector3.new(0, 0.02, 0)) 
+    if RTX.InputManager.Get():isPressed("Jump") then
+        ball:setVelocity(ball:getVelocity() + RTX.Vector3.new(0, 0.02, 0)) 
     end
 end
 
