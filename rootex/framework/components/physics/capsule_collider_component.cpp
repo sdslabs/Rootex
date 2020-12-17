86ee802de364b80682859bde4ea45892ffa2f6c4
Twarit Waikar <wtwarit@gmail.com>
2020-12-18 02:13:08 +0530

    Add collider axis rotation locks Angular factor stores the axis specific multipliers for rotation changes. Setting them to 0 and 1 enables and disables the axis locks respectively. This has been axposed as the setAxisLock(bool) API

--- rootex/framework/components/physics/capsule_collider_component.cpp
+++ rootex/framework/components/physics/capsule_collider_component.cpp
@@ -7,6 +7,7 @@ Component* CapsuleColliderComponent::Create(const JSON::json& capsuleComponentDa
 	    capsuleComponentData.value("radius", 0.5f),
 	    capsuleComponentData.value("sideHeight", 1.0f),
 	    capsuleComponentData.value("material", PhysicsMaterial::Air),
+	    capsuleComponentData.value("angularFactor", Vector3::One),
 	    capsuleComponentData.value("gravity", Vector3(0.0f, -9.8f, 0.0f)),
 	    capsuleComponentData.value("isMoveable", false),
 	    capsuleComponentData.value("isKinematic", false),
@@ -19,8 +20,8 @@ void CapsuleColliderComponent::refreshDimensions()
 	m_CapsuleShape->setImplicitShapeDimensions(btVector3(m_Radius, m_SideHeight / 2.0f, m_Radius));
 }
 
-CapsuleColliderComponent::CapsuleColliderComponent(float radius, float sideHeight, const PhysicsMaterial& material, const Vector3& gravity, bool isMoveable, bool isKinematic, bool generatesHitEvents)
-    : PhysicsColliderComponent(material, DirectX::XM_PI * radius * radius * ((4.0f / 3.0f) * radius + sideHeight), gravity, isMoveable, isKinematic, generatesHitEvents, Ref<btCapsuleShape>(new btCapsuleShape(radius, sideHeight)))
+CapsuleColliderComponent::CapsuleColliderComponent(float radius, float sideHeight, const PhysicsMaterial& material, const Vector3& angularFactor, const Vector3& gravity, bool isMoveable, bool isKinematic, bool generatesHitEvents)
+    : PhysicsColliderComponent(material, DirectX::XM_PI * radius * radius * ((4.0f / 3.0f) * radius + sideHeight), gravity, angularFactor, isMoveable, isKinematic, generatesHitEvents, Ref<btCapsuleShape>(new btCapsuleShape(radius, sideHeight)))
     , m_Radius(radius)
     , m_SideHeight(sideHeight)
 {
--- rootex/framework/components/physics/sphere_collider_component.cpp
+++ rootex/framework/components/physics/sphere_collider_component.cpp
@@ -6,6 +6,7 @@ Component* SphereColliderComponent::Create(const JSON::json& sphereComponentData
 	SphereColliderComponent* component = new SphereColliderComponent(
 		sphereComponentData.value("radius", 1.0f), 
 		sphereComponentData.value("material", PhysicsMaterial::Air),
+	    sphereComponentData.value("angularFactor", Vector3::One),
 	    sphereComponentData.value("gravity", Vector3(0.0f, -9.8f, 0.0f)),
 	    sphereComponentData.value("isMoveable", false),
 	    sphereComponentData.value("isKinematic", false),
@@ -13,8 +14,8 @@ Component* SphereColliderComponent::Create(const JSON::json& sphereComponentData
 	return component;
 }
 
-SphereColliderComponent::SphereColliderComponent(float radius, const PhysicsMaterial& material, const Vector3& gravity, bool isMoveable, bool isKinematic, bool generatesHitEvents)
-    : PhysicsColliderComponent(material, ((4.0f / 3.0f) * DirectX::XM_PI * radius * radius * radius), gravity, isMoveable, isKinematic, generatesHitEvents, Ref<btSphereShape>(new btSphereShape(radius)))
+SphereColliderComponent::SphereColliderComponent(float radius, const PhysicsMaterial& material, const Vector3& angularFactor, const Vector3& gravity, bool isMoveable, bool isKinematic, bool generatesHitEvents)
+    : PhysicsColliderComponent(material, ((4.0f / 3.0f) * DirectX::XM_PI * radius * radius * radius), gravity, angularFactor, isMoveable, isKinematic, generatesHitEvents, Ref<btSphereShape>(new btSphereShape(radius)))
     , m_Radius(radius)
 {
 	m_SphereShape = std::dynamic_pointer_cast<btSphereShape>(m_CollisionShape);
