a8ac8bb4e2029fade9004a315a844379cfc50725
Twarit Waikar <wtwarit@gmail.com>
2021-01-03 03:47:27 +0530

    Fix dialogue graph code to follow order of appearance
    Implements this by maintaining a stack of the dialogue nodes which can
    be returned to in case the answer nodes of QuestionNode reaches the
    terminal. QuestionNode pushes its next chronological node into the stack
    before it order the dialogue to move on to the answer nodes. If the
    answer nodes hit a stop, the stack is checked back for any nodes that
    can be returned to. If no such return values are available, the dialogue
    stops.

---
+++ rootex/script/scripts/common.lua
@@ -0,0 +1,26 @@
+Stack = class("Stack")
+
+function Stack:initialize(list)
+	if not list then
+	  self.stack = {}
+	else
+	  self.stack = list
+	end
+end
+   
+function Stack:push(item)
+	self.stack[#self.stack+1] = item
+end
+
+function Stack:pop()
+	if #self.stack > 0 then
+		return table.remove(self.stack, #self.stack)
+	end
+end
+   
+function Stack:iterator()
+	return function()
+		-- call pop to iterate through all items on a stack
+		return self:pop()
+	end
+end
