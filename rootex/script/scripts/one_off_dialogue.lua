310553174284785ba7fe0e8dedc92ce4b1b8c4e3
Twarit Waikar <wtwarit@gmail.com>
2021-01-04 02:55:53 +0530

    Add character portraits to dialogues

--- rootex/script/scripts/one_off_dialogue.lua
+++ rootex/script/scripts/one_off_dialogue.lua
@@ -5,13 +5,20 @@ OneOffDialogue = class("OneOffDialogue", DialogueNode)
 OneOffDialogue.static.document = rmlui.contexts["default"]:LoadDocument("rootex/script/scripts/one_off_dialogue.rml")
 OneOffDialogue.static.document:Hide()
 
-function OneOffDialogue:initialize(text, exitLogic)
+function OneOffDialogue:initialize(portrait, text, exitLogic)
 	DialogueNode.initialize(self, exitLogic)
 	self.text = text
+	self.portrait = portrait
 end
 
 function OneOffDialogue:getDocument()
 	OneOffDialogue.static.document:GetElementById("dialogue_text").inner_rml = self.text
+	local portraitFormat = [[<img src="../../..://dummy/{src}" width={width}em height={height}em />]]
+	portraitFormat = portraitFormat
+	:gsub("{src}", self.portrait.image)
+	:gsub("{width}", self.portrait.width)
+	:gsub("{height}", self.portrait.height)
+	OneOffDialogue.static.document:GetElementById("dialogue_portrait").inner_rml = portraitFormat
 	return OneOffDialogue.static.document
 end
 
