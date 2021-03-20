5af0a478d6ab5c35f85af909dbf81f7509f624a8
Angad Kambli <angadkambli@gmail.com>
2021-03-20 15:24:27 +0530

    finish porting dialogue_player.lua

--- game/assets/scripts/dialogue/dialogue_player.lua
+++ game/assets/scripts/dialogue/dialogue_player.lua
@@ -19,6 +19,34 @@ function DialoguePlayer:begin(entity)
 end
 
 function DialoguePlayer:update(entity, delta)
+	if self.story:canContinue() then
+		local paragraphs = self.story:continue()
+		self:presentParagraphs(paragraphs)
+		if self.story:canChoose() then
+			self:presentChoices(self.story:getChoices())
+		end
+	end
+
+	if self.story:canChoose() then
+		if RTX.Input.WasPressed("Up") then
+			self.currentChoice = currentChoice - 1
+			if self.currentChoice < 1 then self.currentChoice = self.maxChoices end
+			self:presentChoices(self.story:getChoices())
+		end
+		
+		if RTX.Input.WasPressed("Down") then
+			self.currentChoice = self.currentChoice + 1
+			if self.currentChoice > self.maxChoices then self.currentChoice = 1 end
+			self:presentChoices(self.story:getChoices())
+		end
+		
+		if RTX.Input.WasPressed("Next") then
+			self.story:choose(self.currentChoice)
+			self.currentChoice = 1
+		end
+	else
+		self:getDocument():Hide()
+	end
 end
 
 
@@ -39,43 +67,12 @@ function DialoguePlayer:presentChoices(choices)
 	self.maxChoices = #choices
 	for i, choice in ipairs(choices) do
 		local selection = ""
-		if currentChoice == i then
+		if self.currentChoice == i then
 			selection = "> "
 		end
 		text = text .. "<br />" .. selection .. choice.text
 	end
 	self:getDocument():GetElementById("choices").inner_rml = text
-end
-
-function DialoguePlayer:onUpdate(entity, delta)
-	if self.story:canContinue() then
-		local paragraphs = self.story:continue()
-		presentParagraphs(paragraphs)
-		if self.story:canChoose() then
-			presentChoices(self.story:getChoices())
-		end
-	end
-
-	if self.story:canChoose() then
-		if RTX.InputManager.Get():wasPressed("Up") then
-			currentChoice = currentChoice - 1
-			if currentChoice < 1 then currentChoice = maxChoices end
-			presentChoices(self.story:getChoices())
-		end
-		
-		if RTX.InputManager.Get():wasPressed("Down") then
-			currentChoice = currentChoice + 1
-			if currentChoice > maxChoices then currentChoice = 1 end
-			presentChoices(self.story:getChoices())
-		end
-		
-		if RTX.InputManager.Get():wasPressed("Next") then
-			self.story:choose(currentChoice)
-			currentChoice = 1
-		end
-	else
-		self:getDocument():Hide()
-	end
 end
 
 function DialoguePlayer:onEnd(entity)
