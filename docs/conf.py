a903757dc6afbd0666ec8452ee7e8dff80921b6f
Twarit Waikar <wtwarit@gmail.com>
2021-03-21 02:26:36 +0530

    Fix minor crash

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'Rootex'
-copyright = 'MIT Licence, SDSLabs'
+copyright = 'MIT License, SDSLabs'
 author = 'SDSLabs'
 master_doc = 'index'
 
