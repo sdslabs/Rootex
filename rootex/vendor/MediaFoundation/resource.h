d062a6c25667ebe761513664ae96336e8951ba38
Ishan-002 <ishan26rawat@gmail.com>
2021-04-30 11:37:30 +0530

    Add a simple video player based on Windows Media Foundation

---
+++ rootex/vendor/MediaFoundation/resource.h
@@ -0,0 +1,8 @@
+#define IDD_MFPLAYBACK_DIALOG           102
+#define IDM_EXIT                        105
+#define IDC_MFPLAYBACK                  109
+#define IDD_OPENURL                     129
+#define IDC_EDIT_URL                    1000
+#define ID_FILE_OPENFILE                32771
+#define ID_FILE_OPENURL                 32772
+#define IDC_STATIC                      -1
