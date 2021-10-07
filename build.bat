7a7d136a09e02259a85b333e35fa568bf64aef2a
Angad Kambli <angadkambli@gmail.com>
2021-10-07 23:18:06 +0530

    add basic subcommands to build.bat
    Add basic utility subcommads to change submodule repo
    Will change the url of game-assets later to point to the correct repo

--- build.bat
+++ build.bat
@@ -1,2 +1,21 @@
-cd build/
-cmake --build .
+@echo off
+IF "%1"=="" (
+    cd build/
+    cmake --build .
+)
+IF "%1"=="assets-test" (
+    git submodule deinit -f game/assets
+    rm -rf game/assets
+    rm -rf .git/modules/assets
+    git config --file=.gitmodules submodule.assets.url https://gitlab.com/sdslabs/rootex-assets.git
+    git config --file=.gitmodules submodule.assets.branch main
+    git submodule update --init --recursive --remote
+)
+IF "%1"=="assets-game" (
+    git submodule deinit -f game/assets
+    rm -rf game/assets
+    rm -rf .git/modules/assets
+    git config --file=.gitmodules submodule.assets.url https://github.com/angad-k/Test-Module.git
+    git config --file=.gitmodules submodule.assets.branch master
+    git submodule update --init --recursive --remote
+)
