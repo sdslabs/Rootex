2e4b8f6e7a065e44f753d80d35d71351ccc8e6f6
Angad Kambli <angadkambli@gmail.com>
2021-10-13 14:23:44 +0530

    replace test repo in script with actual Rootex Game Assets repo

--- build.bat
+++ build.bat
@@ -15,7 +15,7 @@ IF "%1"=="assets-game" (
     git submodule deinit -f game/assets
     rm -rf game/assets
     rm -rf .git/modules/assets
-    git config --file=.gitmodules submodule.assets.url https://github.com/angad-k/Test-Module.git
-    git config --file=.gitmodules submodule.assets.branch master
+    git config --file=.gitmodules submodule.assets.url https://gitlab.com/sdslabs/rootex-game.git
+    git config --file=.gitmodules submodule.assets.branch main
     git submodule update --init --recursive --remote
 )
