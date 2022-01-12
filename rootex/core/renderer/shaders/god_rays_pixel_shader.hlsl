a46c07167f74445cdaf200f7d41e38ebb35beb66
Priyansh Rathi <techiepriyansh@gmail.com>
2022-01-12 18:29:31 +0530

    god rays shader

--- rootex/core/renderer/shaders/god_rays_pixel_shader.hlsl
+++ rootex/core/renderer/shaders/god_rays_pixel_shader.hlsl
@@ -1,3 +1,5 @@
+#define THRESHOLD 0.95f
+
 cbuffer cbGodRays : register(b0)
 {
 	float3 sunScreenSpacePos;
@@ -19,8 +21,31 @@ struct GodRaysVSOutput
 
 float4 main(GodRaysVSOutput input) : SV_TARGET
 {
-	float4 rgba = InputTexture.Sample(SampleType, input.tex);
-	float lum = (0.2126f * rgba.r) + (0.7152f * rgba.g) + (0.0722f * rgba.b);
+	float2 texCoord = input.tex.xy;
 
-	return float4(lum, lum, lum, rgba.a);
+	float2 deltaTexCoord = input.tex - sunScreenSpacePos.xy;
+	deltaTexCoord *= (1.0f / float(numSamples)) * density;
+
+	float3 color = smoothstep(THRESHOLD, 1.0f, InputTexture.Sample(SampleType, texCoord).xyz);
+
+	float illumination = 1.0f;
+
+	for (int i = 0; i < numSamples; i++)
+	{
+		texCoord -= deltaTexCoord;
+		float4 rawSmpl = InputTexture.Sample(SampleType, texCoord);
+		float3 smpl = smoothstep(THRESHOLD, 1.0f, rawSmpl.xyz);
+		smpl *= illumination * weight * rawSmpl.a;
+		color += smpl;
+		illumination *= decay;
+	}
+
+	float lum = color.g * exposure - 0.2f;
+
+	float4 origColor = InputTexture.Sample(SampleType, input.tex);
+	float4 raysColor = float4(lum, lum, lum, 1.0f);
+
+	float4 finalColor = lerp(origColor * 1.2 - float4(0.2f, 0.2f, 0.2f, 0.2f), raysColor, 0.3f);
+
+	return float4(finalColor.xyz, 1.0);
 }
