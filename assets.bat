966c918e5c9323b7a74b48fd5ab3e8f5d9140cf2
Angad Kambli <angadkambli@gmail.com>
2022-01-17 12:43:23 +0530

    use https for cloning url and set ssh as upstream

--- assets.bat
+++ assets.bat
@@ -3,23 +3,21 @@ IF "%1"=="assets-test" (
     git submodule deinit -f game/assets
     rm -r --force .\game\assets
     rm -r --force .\.git\modules\assets
-    IF "%2"=="https" (
-        git config --file=.gitmodules submodule.assets.url https://gitlab.com/sdslabs/rootex-assets.git
-    ) ELSE (
-        git config --file=.gitmodules submodule.assets.url git@gitlab.com:sdslabs/rootex-assets.git
-    )
+    git config --file=.gitmodules submodule.assets.url https://gitlab.com/sdslabs/rootex-assets.git
     git config --file=.gitmodules submodule.assets.branch main
+    git submodule update --init --recursive --remote
+    cd game/assets
+    git remote add upstream git@gitlab.com:sdslabs/rootex-assets.git
 )
 IF "%1"=="assets-game" (
     git submodule deinit -f game/assets
     rm -r --force .\game\assets
     rm -r --force .git\modules\assets
-    IF "%2"=="https" (
-        git config --file=.gitmodules submodule.assets.url https://gitlab.com/sdslabs/rootex-game.git
-    ) ELSE (
-        git config --file=.gitmodules submodule.assets.url git@gitlab.com:sdslabs/rootex-game.git
-    )
+    git config --file=.gitmodules submodule.assets.url https://gitlab.com/sdslabs/rootex-game.git
     git config --file=.gitmodules submodule.assets.branch main
+    git submodule update --init --recursive --remote
+    cd game/assets
+    git remote add upstream git@gitlab.com:sdslabs/rootex-game.git
 )
 
 IF "%1"=="assets-custom" (
@@ -28,5 +26,5 @@ IF "%1"=="assets-custom" (
     rm -r --force .git\modules\assets
     git config --file=.gitmodules submodule.assets.url %2
     git config --file=.gitmodules submodule.assets.branch main
+    git submodule update --init --recursive --remote
 )
-echo Submodule updated. Run 'git submodule update --init --recursive --remote' for changes to reflect in game/assets
