a2d7cee2995b5d04694bb8c69b9df695e0a51933
Bashar Ahmed <bashar_a@cs.iitr.ac.in>
2022-01-18 15:03:00 +0530

    enable resume thorugh UI button

--- game/assets/scripts/pause.lua
+++ game/assets/scripts/pause.lua
@@ -5,18 +5,30 @@ function Pause:initialize(entity)
         pauseScene = "Enter scene to pause"
     }
     self.running = false
+    
 end
 
 function Pause:enterScene(entity)
     entity:bind("InputPause", Pause.onPauseLevel)
 end
 
+function Pause:Resume(entity)
+    RTX.GetCurrentScene():removeChild(RTX.Scene.FindScenesByName("pause")[1])
+    self.running = false
+    RTX.System.unPause()
+end
+
 function Pause:onPauseLevel(entity, event)
+    ::start::
     if event:getData().y == 1 and self.running == false then
         RTX.GetCurrentScene():addChild(RTX.Scene.CreateFromFile("game/assets/scenes/pauseUI.scene.json"))
         self.running = true
         RTX.System.pause()
     elseif event:getData().y == 1 and self.running == true then
+        if((#RTX.Scene.FindScenesByName("pause"))==0) then
+            self.running=false
+            goto start
+        end
         RTX.GetCurrentScene():removeChild(RTX.Scene.FindScenesByName("pause")[1])
         self.running = false
         RTX.System.unPause()
