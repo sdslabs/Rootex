e6829a71358670adaf7fc7224f01d9476514232a
Bashar Ahmed <bashar_a@cs.iitr.ac.in>
2022-01-18 13:04:16 +0530

    enable pause for additional Systems

--- rootex/framework/systems/trigger_system.cpp
+++ rootex/framework/systems/trigger_system.cpp
@@ -16,32 +16,69 @@ TriggerSystem* TriggerSystem::GetSingleton()
 
 void TriggerSystem::update(float deltaMilliseconds)
 {
-	for (auto& trigger : ECSFactory::GetAllTriggerComponent())
+	if (!(m_IsSystemPaused))
 	{
-		trigger.openRegister();
+		for (auto& trigger : ECSFactory::GetAllTriggerComponent())
 		{
-			btGhostObject* triggerGhost = trigger.getGhostObject();
-			for (int i = 0; i < triggerGhost->getNumOverlappingObjects(); i++)
+			trigger.openRegister();
 			{
-				if (trigger.canNotifyEntry())
+				btGhostObject* triggerGhost = trigger.getGhostObject();
+				for (int i = 0; i < triggerGhost->getNumOverlappingObjects(); i++)
 				{
-					trigger.notifyEntry();
+					if (trigger.canNotifyEntry())
+					{
+						trigger.notifyEntry();
+					}
+					CollisionComponent* entrantComponent = (CollisionComponent*)triggerGhost->getOverlappingObject(i)->getUserPointer();
+					trigger.registerEntry(entrantComponent->getOwner().getID());
 				}
-				CollisionComponent* entrantComponent = (CollisionComponent*)triggerGhost->getOverlappingObject(i)->getUserPointer();
-				trigger.registerEntry(entrantComponent->getOwner().getID());
-			}
 
-			int exitCount = trigger.findExitCount();
-			for (int i = 0; i < exitCount; i++)
-			{
-				if (trigger.canNotifyExit())
+				int exitCount = trigger.findExitCount();
+				for (int i = 0; i < exitCount; i++)
 				{
-					trigger.notifyExit();
+					if (trigger.canNotifyExit())
+					{
+						trigger.notifyExit();
+					}
 				}
+			}
+			trigger.closeRegister();
+
+			trigger.updateTransform();
+		}
+	}
+	else
+	{
+		for (auto& trigger : ECSFactory::GetAllTriggerComponent())
+		{
+			if (!(trigger.getOwner().getScene()->getIsScenePaused()))
+			{
+				trigger.openRegister();
+				{
+					btGhostObject* triggerGhost = trigger.getGhostObject();
+					for (int i = 0; i < triggerGhost->getNumOverlappingObjects(); i++)
+					{
+						if (trigger.canNotifyEntry())
+						{
+							trigger.notifyEntry();
+						}
+						CollisionComponent* entrantComponent = (CollisionComponent*)triggerGhost->getOverlappingObject(i)->getUserPointer();
+						trigger.registerEntry(entrantComponent->getOwner().getID());
+					}
+
+					int exitCount = trigger.findExitCount();
+					for (int i = 0; i < exitCount; i++)
+					{
+						if (trigger.canNotifyExit())
+						{
+							trigger.notifyExit();
+						}
+					}
+				}
+				trigger.closeRegister();
+
+				trigger.updateTransform();
 			}
 		}
-		trigger.closeRegister();
-
-		trigger.updateTransform();
 	}
 }
