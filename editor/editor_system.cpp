0b4b4d0f869d865e0a274ad98fa68a3aa95eae45
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2022-02-04 16:47:56 +0530

    autosave-toggle

--- editor/editor_system.cpp
+++ editor/editor_system.cpp
@@ -458,6 +458,8 @@ void EditorSystem::drawDefaultUI(float deltaMilliseconds)
 			}
 			if (ImGui::BeginMenu("Scene"))
 			{
+				ImGui::Checkbox("Autosave", &m_Autosave);
+
 				if (SceneLoader::GetSingleton()->getCurrentScene())
 				{
 					SceneLoader::GetSingleton()->getCurrentScene()->getSettings().draw();
@@ -859,9 +861,12 @@ Variant EditorSystem::saveAll(const Event* event)
 
 Variant EditorSystem::autoSave(const Event* event)
 {
-	PRINT("Auto-saving current scene...");
-	saveAll(nullptr);
-	return true;
+	if (m_Autosave)
+	{
+		PRINT("Auto-saving current scene...");
+		saveAll(nullptr);
+		return true;
+	}
 }
 
 Variant EditorSystem::saveBeforeQuit(const Event* event)
